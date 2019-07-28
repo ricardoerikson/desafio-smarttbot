5ba2b7fc2ee00a53792eb789909e7c251d7ca2eb
Ricardo Erikson <ricardoerikson@gmail.com>
2019-07-28 12:15:06 -0300

    # This is a combination of 4 commits.
    wip: add a wrapper class with methods for consuming poloniex api
    
    add a wrapper class with methods that encapsulate asynchronous http calls to poloniex http public
    methods
    
    wip: remove async requet from poloniex wrapper
    
    wip: add method to determine optimal period for request
    
    wip: add utility functions to validate and convert periods

---
+++ chart_data/utils.py
@@ -0,0 +1,43 @@
+import re
+
+def validate_period_format(period_str):
+	"""validates a period in string format
+
+	Args:
+	    period_str (string): period in string format starting with a
+	    number and ending with the letters m, h or d, for minutes, hour and day,
+	    respectively.
+
+	Returns:
+	    bool: True if the period is valid, else False
+	"""
+	regex = r"^[0-9]+[hmd]$"
+	result = re.search(regex, period_str, re.MULTILINE)
+	return False if result is None else True
+
+def convert_period_into_seconds(period_str):
+	"""converts a given period of time (in string format) into seconds
+
+	Args:
+	    period_str (string): period of time represented by a number followed by either
+	    of the letters 'm', 'h' or 'd', corresponding to minutes, hour and day, respectively.
+
+	Returns:
+	    int: period of time in seconds
+
+	Raises:
+	    ValueError: raised if the period is represented in minutes and it's not a multiple of 5.
+	"""
+	number = int(''.join(filter(str.isdigit, period_str)))
+	letter = ''.join(filter(str.isalpha, period_str))
+
+	if (letter is 'm') and (number % 5 is not 0):
+		raise ValueError('If you are using "minutes", the period must be multiple of 5.')
+
+	if letter is 'm':
+		return number * 60
+	if letter is 'h':
+		return number * 60 * 60
+
+	# if period is given in days (d)
+	return number * 60 * 60 * 24
